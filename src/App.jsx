85a9388a0e39133d0427808817c617d869a016ef
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 19:39:25 +0430

    import all techStack svg file in app comp

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import "./components/Navbar";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+
+import html from "./assets/img/skills/html-5.svg";
+import css from "./assets/img/skills/css-3.svg";
+import js from "./assets/img/skills/js.svg";
+import sass from "./assets/img/skills/sass.svg";
+import bootstrap from "./assets/img/skills/bootstrap.svg";
+import tailwind from "./assets/img/skills/tailwind.svg";
+import react from "./assets/img/skills/react.svg";
+import mysql from "./assets/img/skills/mysql.svg";
+import git from "./assets/img/skills/git.svg";
+import wordpress from "./assets/img/skills/wordpress.png";
+
 function App() {
   return (
     <>
       <Navbar />
       <Header message="Welcome to my Personal Website!" />
+      <Projects />
       <Footer />
     </>
   );
