d51a8f70ba76d5224fb2b8b98f7e7673c7b9058a
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-18 00:34:34 +0430

    import and use Skills comp in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import "./components/Navbar";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 
@@ -40,6 +40,7 @@ function App() {
       <Navbar />
       <Header message="Welcome to my Personal Website!" />
       <About animation={animation} />
+      <Skills />
       <Projects techStacks={techStacks} />
       <Footer />
     </>
