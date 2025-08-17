7b829509a1c5d6b84f8de6936412125121acd6fb
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 15:37:01 +0430

    import and use Footer component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import "./components/Navbar";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Navbar />
       <Header message="Welcome to my Personal Website!" />
+      <Footer />
     </>
   );
 }
