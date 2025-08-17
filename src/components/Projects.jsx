ce4267cef2da459cd87bca5ee147afdeac57d4cf
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 18:27:52 +0430

    Create an array of project objects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,3 +1,50 @@
-export function Projects() {
-  return <p>projects section</p>;
+function Projects() {
+  const projects = [
+    {
+      name: "omnifood",
+      image: "../assets/img/Projects/omnifood.png",
+      descipriton: "a custom healthy meal delivered website.",
+      codeLink: "https://github.com/HanifaTavassoli/Omnifood",
+      liveUrl: "https://hanifa-omnifood.netlify.app",
+      featured: true,
+    },
+    {
+      name: "portfolio",
+      image: "../assets/img/Projects/omnifood.png",
+      descipriton:
+        "A personal portfolio website to showcase my skills and projects.",
+      codeLink: "https://github.com/HanifaTavassoli/Portfolio",
+      liveUrl: "https://hanifatavassoli-portfolio.netlify.app",
+      featured: true,
+    },
+    {
+      name: "Natours",
+      image: "../assets/img/Projects/natours.png",
+      descipriton:
+        "A custom nature tour experience website for adventurous travelers.",
+      codeLink: "https://github.com/HanifaTavassoli/Natours",
+      liveUrl: "https://hanifa-natours.netlify.app",
+      featured: true,
+    },
+    {
+      name: "Furnivo",
+      image: "../assets/img/Projects/furnivo-homepage.png",
+      descipriton: "A WordPress site showcasing stylish furniture collections.",
+      codeLink: "#",
+      liveUrl: "https://dahlia-finalproject.afghanpathways.com",
+      featured: true,
+    },
+    {
+      name: "Travana",
+      image: "../assets/img/Projects/travana.png",
+      descipriton:
+        "A custom travel service with personalized luxury vacations.",
+      codeLink: "#",
+      liveUrl: "https://dahlia.afghanpathways.com",
+      featured: false,
+    },
+  ];
+  return <h1>Pojects section</h1>;
 }
+
+export default Projects;
