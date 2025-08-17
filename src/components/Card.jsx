2afa48a7ab1dcdd952d56a9cc9c5e7a14db50ba1
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 18:25:16 +0430

    Implement props for dynamic content in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,31 +2,31 @@ function Card({ image, name, description, liveUrl, codeLink, featured }) {
   return (
     <>
       <div class="bg-neutral-800 rounded-sm overflow-hidden w-full max-w-[320px]">
-        <a href="https://hanifa-omnifood.netlify.app" class="">
+        <a href={liveUrl} class="">
           <img
-            src="img/Projects/omnifood.png"
-            alt="omnifood project"
+            src={image}
+            alt="{name} project"
             class="transition-transform duration-500 hover:scale-105"
           />
         </a>
         <div class="text-start p-5">
-          <h3 class="text-xl font-bold mb-2">omnifood</h3>
+          <h3 class="text-xl font-bold mb-2">{name}</h3>
           <div class="flex flex-row mb-4 gap-3">
             <img src="img/skills/html-5.svg" alt="html logo" class="w-7" />
             <img src="img/skills/css-3.svg" alt="css-3 logo" class="w-7" />
             <img src="img/skills/js.svg" alt="js logo" class="w-7" />
           </div>
-          <p class="mb-8">a custom healthy meal delivered website.</p>
+          <p class="mb-8">{description}</p>
           <div class="flex flex-wrap sm:flex-nowrap sm:flex-row gap-4 items-center justify-center text-sm">
             <a
-              href="https://github.com/HanifaTavassoli/Omnifood"
+              href={codeLink}
               target="_blank"
               class="px-8 py-2 rounded-xs bg-slate-700 text-sky-500 hover:bg-slate-900"
             >
               <i class="fa-brands fa-github"></i> Github
             </a>
             <a
-              href="https://hanifa-omnifood.netlify.app"
+              href={liveUrl}
               target="_blank"
               class="px-8 py-2 rounded-sm bg-slate-700 text-sky-500 hover:bg-slate-900"
             >
