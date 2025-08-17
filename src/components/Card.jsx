7595f9f528ea09dbf2c2361d8b4cb04200befcec
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 22:01:49 +0430

    convert class to className

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,36 +1,32 @@
 function Card({ image, name, description, liveUrl, codeLink, featured }) {
   return (
     <>
-      <div class="bg-neutral-800 rounded-sm overflow-hidden w-full max-w-[320px]">
-        <a href={liveUrl} class="">
+      <div className="bg-neutral-800 rounded-sm overflow-hidden w-full max-w-[320px]">
+        <a href={liveUrl} className="">
           <img
             src={image}
-            alt="{name} project"
-            class="transition-transform duration-500 hover:scale-105"
+            alt={`${name} project`}
+            className="transition-transform duration-500 hover:scale-105"
           />
         </a>
-        <div class="text-start p-5">
-          <h3 class="text-xl font-bold mb-2">{name}</h3>
-          <div class="flex flex-row mb-4 gap-3">
-            <img src="img/skills/html-5.svg" alt="html logo" class="w-7" />
-            <img src="img/skills/css-3.svg" alt="css-3 logo" class="w-7" />
-            <img src="img/skills/js.svg" alt="js logo" class="w-7" />
-          </div>
-          <p class="mb-8">{description}</p>
-          <div class="flex flex-wrap sm:flex-nowrap sm:flex-row gap-4 items-center justify-center text-sm">
+        <div className="text-start p-5">
+          <h3 className="text-xl font-bold mb-2">{name}</h3>
+          <div className="flex flex-row mb-4 gap-3"></div>
+          <p className="mb-8">{description}</p>
+          <div className="flex flex-wrap sm:flex-nowrap sm:flex-row gap-4 items-center justify-center text-sm">
             <a
               href={codeLink}
               target="_blank"
-              class="px-8 py-2 rounded-xs bg-slate-700 text-sky-500 hover:bg-slate-900"
+              className="px-8 py-2 rounded-xs bg-slate-700 text-sky-500 hover:bg-slate-900"
             >
-              <i class="fa-brands fa-github"></i> Github
+              <i className="fa-brands fa-github"></i> Github
             </a>
             <a
               href={liveUrl}
               target="_blank"
-              class="px-8 py-2 rounded-sm bg-slate-700 text-sky-500 hover:bg-slate-900"
+              className="px-8 py-2 rounded-sm bg-slate-700 text-sky-500 hover:bg-slate-900"
             >
-              <i class="fa fa-link"></i>
+              <i className="fa fa-link"></i>
               Visit
             </a>
           </div>
