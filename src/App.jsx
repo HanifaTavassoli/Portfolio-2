a89c5481ac69dd2d7e890a9c398a8eda565c398f
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 11:27:11 +0430

    pass message props to Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import "./components/Navbar";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1>Hanifa Tavassoli</h1>
+      <Navbar />
+      <Header message="Welcome to my Personal Website!" />
     </>
   );
 }
