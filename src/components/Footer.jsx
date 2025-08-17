8fe1cb7a6966a6b7c5219668b30bbef70c6180d8
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 15:04:54 +0430

    add jsx for Footer component with logo and grid layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,20 @@
-export function Footer() {
-  return <p>Footer section</p>;
+import logo from "../assets/img/logo.png";
+function Footer() {
+  return (
+    <footer class="bg-neutral-950 py-16">
+      <div class="custom-container grid grid-rows-3 gap-3 justify-items-center text-white text-center">
+        <div>
+          <a href="#">
+            <img
+              src={logo}
+              alt="logo"
+              class="h-16 rounded-full border-2 border-neutral-950"
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
+
+export default Footer;
