655405a2e2871566767728ceea9a457631141400
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-17 10:18:40 +0430

    Add mobile navigation links with hover effects to menu panel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -110,6 +110,29 @@ function Navbar() {
             </svg>
           </button>
         </div>
+
+        <ul className="flex flex-col items-start gap-y-6 px-6 text-white">
+          <li>
+            <a href="#hero-section" className="hover:text-sky-500">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#skills-section" className="hover:text-sky-500">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#projects-section" className="hover:text-sky-500">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contacts-section" className="hover:text-sky-500">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
     </>
   );
