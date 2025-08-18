111ff76d43824b36293d5e5bb73f1d6e73869ff3
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-18 16:34:35 +0430

    Set up Skills section structure in Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,20 @@
 function Skills() {
-  return <h2>Skill section</h2>;
+  return (
+    <>
+      <section
+        id="skills-section"
+        className="py-20 bg-neutral-800 overflow-hidden"
+      >
+        <div className="custom-container py-5 mx-auto text-center">
+          <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-12 text-white">
+            My Web Skills
+          </h2>
+
+          <div className="flex space-x-16 overflow-hidden p-5"></div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Skills;
