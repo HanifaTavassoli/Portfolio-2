f682ca89de034b9e6a20cf127c5b2ac1d954dc25
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-18 16:49:16 +0430

    Rendered the tech stack list twice for getting animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -25,6 +25,20 @@ function Skills({ techStacks }) {
                 </li>
               ))}
             </ul>
+
+            {/* <!-- render two times manually --> */}
+            <ul className="flex space-x-16 animation">
+              {stacks.map((stack) => (
+                <li className="shadow-[0_0_5px_0.5px] shadow-sky-500/50 border-1 border-sky-500 rounded-md p-5 transition-transform duration-300 hover:scale-120 hover:shadow-[0_0_5px_2.5px]">
+                  <img
+                    src={stack}
+                    alt={`${stack} logo`}
+                    className="max-w-25"
+                    width="50"
+                  />
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </section>
