cf6855b16c8bc82d9c01aaec7ea94aa8cde76794
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-24 19:12:28 +0430

    Create ContactForm component and add contact details section

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,40 @@
+function ContactForm() {
+  return (
+    <>
+      <section id="contacts-section" className="py-5 bg-neutral-800">
+        <div className="custom-container py-20 mx-auto text-center">
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-bold mb-10">
+            Contact Me
+          </h3>
+          <div className="flex flex-col md:flex-row gap-10 justify-center items-start md:items-center w-full px-4">
+            <div className="w-full md:basis-1/3 flex flex-col gap-10">
+              <div className="flex gap-6 items-start">
+                <i className="fa-duotone fa-solid fa-phone text-xl p-6 rounded-sm bg-sky-800"></i>
+                <p className="flex flex-col text-start gap-2">
+                  <span className="font-bold text-xl">Phone Number</span>
+                  <span>+93 (0)781372682</span>
+                </p>
+              </div>
+              <div className="flex gap-8">
+                <i className="fa-solid fa-envelope text-xl p-6 rounded-sm bg-sky-800"></i>
+                <p className="flex flex-col text-start gap-2 text-wrap">
+                  <span className="font-bold text-xl">Email</span>
+                  <span className="text-wrap">hanifatavassoli@gmail.com</span>
+                </p>
+              </div>
+              <div className="flex gap-8">
+                <i className="fa-solid fa-home text-xl p-6 rounded-sm bg-sky-800"></i>
+                <p className="flex flex-col text-start gap-2">
+                  <span className="font-bold text-xl">Location</span>
+                  <span>Herat, Afghanistan</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ContactForm;
