0c3229da4ed7104f1035a3db9d4c38ff75185511
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-27 06:00:48 +0430

    import all techs and projects image

---
+++ src/data.jsx
@@ -0,0 +1,17 @@
+import omnifood from "../assets/img/Projects/omnifood.png";
+import portfolio from "../assets/img/Projects/portfolio.png";
+import natours from "../assets/img/Projects/natours.png";
+import calculator from "../assets/img/Projects/calculator.png";
+import travana from "../assets/img/Projects/travana.png";
+import furnivo from "../assets/img/Projects/furnivo-homepage.png";
+
+import html from "./assets/img/skills/html-5.svg";
+import css from "./assets/img/skills/css-3.svg";
+import js from "./assets/img/skills/js.svg";
+import sass from "./assets/img/skills/sass.svg";
+import bootstrap from "./assets/img/skills/bootstrap.svg";
+import tailwind from "./assets/img/skills/tailwind.svg";
+import react from "./assets/img/skills/react.svg";
+import mysql from "./assets/img/skills/mysql.svg";
+import git from "./assets/img/skills/git.svg";
+import wordpress from "./assets/img/skills/wordpress.png";
