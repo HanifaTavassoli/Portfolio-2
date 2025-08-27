e082a58c8aaa9bfd6a10997930b061503925db23
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-27 06:09:16 +0430

    some updates

--- src/data.jsx
+++ src/data.jsx
@@ -1,9 +1,9 @@
-import omnifood from "../assets/img/Projects/omnifood.png";
-import portfolio from "../assets/img/Projects/portfolio.png";
-import natours from "../assets/img/Projects/natours.png";
-import calculator from "../assets/img/Projects/calculator.png";
-import travana from "../assets/img/Projects/travana.png";
-import furnivo from "../assets/img/Projects/furnivo-homepage.png";
+import omnifood from "./assets/img/Projects/omnifood.png";
+import portfolio from "./assets/img/Projects/portfolio.png";
+import natours from "./assets/img/Projects/natours.png";
+import calculator from "./assets/img/Projects/calculator.png";
+import travana from "./assets/img/Projects/travana.png";
+import furnivo from "./assets/img/Projects/furnivo-homepage.png";
 
 import html from "./assets/img/skills/html-5.svg";
 import css from "./assets/img/skills/css-3.svg";
