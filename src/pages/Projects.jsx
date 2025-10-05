19f0acc0554e392cbd3a67a654bb18cb0d4b09cb
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-10-05 14:25:16 +0430

    created and integrate Projects comp

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,61 @@
+import Card from "../components/Card";
+import { projects } from "../data";
+import ProjectFilterBar from "../components/ProjectFilterBar";
+import { useState } from "react";
+import Layout from "./Layout";
+
+function Projects({ isDark, toggleTheme }) {
+  const techs = [
+    "all",
+    "css",
+    "js",
+    "sass",
+    "bootstrap",
+    "tailwind",
+    "react",
+    "wordpress",
+  ];
+
+  const [filteredProj, setFilteredProj] = useState(projects);
+
+  const onFilter = (tech) => {
+    if (tech == "all") {
+      setFilteredProj(projects);
+    } else {
+      const filteredProjects = projects.filter((proj) =>
+        Object.keys(proj.stacks).includes(tech)
+      );
+      setFilteredProj(filteredProjects);
+    }
+  };
+  return (
+    <Layout isDark={isDark} toggleTheme={toggleTheme}>
+      <section
+        id="projects-section"
+        className="py-20 mt-5 bg-gray-200 text-black dark:bg-neutral-800 dark:text-white"
+      >
+        <div className="custom-container py-5 px-4 text-center mx-auto">
+          <h2 className="text-xl sm:text-2xl md:text-3xl font-bold mb-[48px]">
+            My Projects
+          </h2>
+
+          <ProjectFilterBar techs={techs} onFilter={onFilter} />
+
+          <div className="grid gap-x-0 md:gap-x-8 gap-y-8 grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center mx-auto">
+            {filteredProj.length == 0 ? (
+              <div className="col-span-full text-left text-black dark:text-white w-full mx-auto">
+                <p className="text-3xl font-semibold py-8 px-6 bg-gray-100 dark:bg-neutral-700 border-l-8 border-sky-500/50 shadow-sm rounded-lg">
+                  No projects found
+                </p>
+              </div>
+            ) : (
+              filteredProj.map((project) => <Card project={{ ...project }} />)
+            )}
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default Projects;
