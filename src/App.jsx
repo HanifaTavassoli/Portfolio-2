b1cf494200806c097947b14c889cfcc59f957502
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-10-05 14:23:46 +0430

    add routing and navigation for project's pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import ContactForm from "./components/ContactForm";
-import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 import animation from "./assets/img/animation.gif";
+import NotFound from "./pages/NotFound";
+import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
   const [isDark, setIsDark] = useState(
@@ -33,15 +34,46 @@ function App() {
     });
   };
   return (
-    <>
-      <Navbar isDark={isDark} toggleTheme={toggleTheme} />
-      <Header message="Welcome to my Personal Website!" isDark={isDark} />
-      <About animation={animation} />
-      <Skills />
-      <Projects />
-      <ContactForm />
-      <Footer isDark={isDark} />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home isDark={isDark} toggleTheme={toggleTheme} />}
+        />
+        <Route
+          path="/about"
+          element={
+            <About
+              isDark={isDark}
+              toggleTheme={toggleTheme}
+              animation={animation}
+            />
+          }
+        />
+        <Route
+          path="/contact"
+          element={<Contact isDark={isDark} toggleTheme={toggleTheme} />}
+        />
+        <Route
+          path="/projects"
+          element={<Projects isDark={isDark} toggleTheme={toggleTheme} />}
+        />
+        <Route
+          path="/projects/:projectId"
+          element={<ProjectDetails isDark={isDark} toggleTheme />}
+        />
+        <Route
+          path="/*"
+          element={<NotFound isDark={isDark} toggleTheme={toggleTheme} />}
+        />
+
+        {/* <About animation={animation} />
+        <Skills />
+        <Projects />
+        <ContactForm />
+        <Footer isDark={isDark} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
